ac54265f4edba317c17db04069e209c66e680cf2
Andoni Salcedo Navarro <andonisalcedo@gmail.com>
2023-05-24 15:29:05 +0200

    deployworks

--- RESOURCES/deploy/k8s/stop.sh
+++ RESOURCES/deploy/k8s/stop.sh
@@ -1,4 +1,3 @@
-kubectl delete -f bash.yml --force
 kubectl delete -f mongo-statefulset.yml 
 kubectl delete -f rabbit-deployment.yml
 kubectl delete -f mysql-statefulset.yml 
--- start.sh
+++ start.sh
@@ -2,9 +2,16 @@
 
 BASE_DIR=$(pwd)
 
-for file in $(ls -d $BASE_DIR/*) 
+if [ -d "$BASE_DIR/RESOURCES" ]; then
+    cd "$BASE_DIR/RESOURCES"
+    bash start.sh
+    cd "$BASE_DIR"
+fi
+
+# Procesar todos los demás directorios
+for file in $(ls -d $BASE_DIR/*)
 do
-    if [ -d "$file" ]; then
+    if [ -d "$file" ] && [ "$file" != "$BASE_DIR/RESOURCES" ]; then
         cd "$file"
         bash start.sh
         cd "$BASE_DIR"
